efb1e07648f84f4405632f3272d5dcb516094aef
Dillon Nguyen <dillonkn@stanford.edu>
2024-01-21 21:43:29 -0800

    Completed basic calculator functionality

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const displayValue = "";
-const firstNum = "";
-const secondNum = "";
-const operator = "";
-const currStep = "firstNum";
+let displayValue = "";
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+let currStep = "firstNum";
+
+const displayScreen = document.querySelector('#screen');
+const operators = "+-×÷=";
+const decimal = "."
 
 const main = () => {
     makeEventListeners();
 
 }
+
 
 // Create event listeners for all buttons
 const makeEventListeners = () => {
@@ -17,25 +22,118 @@ const makeEventListeners = () => {
     // Add 'onclick' event listeners for all buttons
     buttons.forEach((element) => {
         element.addEventListener('click', parseInput);
-    }
+    })
 }
+
 
 // Parse input on click
 const parseInput = (e) => {
+    // Store input in variable
     const input = e.target.innerText;
-    if (currStep === "firstNum") {
 
+    if (input === "CLEAR") {
+        clearData();
+        updateDisplay();
     }
-    if (currStep === "operator") {
+    else if (currStep === "firstNum") {
+        if (input === "DELETE" && firstNum.length > 0) {
+            firstNum = firstNum.slice(0, -1);
+            displayValue = firstNum;
+            updateDisplay();
+        }
+        // If input is a number and firstNum is not overloaded
+        else if (!isNaN(input) && firstNum.length < 9) {
+            firstNum += input;
+            displayValue = firstNum;
+            updateDisplay();
+        }
+        // If input is a valid operator and we have numbers, change to operator step
+        else if (operators.includes(input)) {
+            if (input !== "=" && displayValue.length > 0) {
+                console.log('test');
+                currStep = "operator";
+                operator = input;
+            }
+        }
+    }
+    else if (currStep === "operator") {
+        // If input is another operator that isn't "=", change operator to operator
+        if (operators.includes(input) && input !== "=") {
+            operator = input;
+        }
+        // If input is a number
+        else if (!isNaN(input)) {
+            currStep = "secondNum";
+            secondNum = input;
+            displayValue = secondNum;
+            updateDisplay();
+        }
+    }
+    // Current step is secondNum
+    else {
+        if (input === "DELETE" && firstNum.length > 0) {
+            firstNum = firstNum.slice(0, -1);
+            displayValue = firstNum;
+            updateDisplay();
+        }
+        // If input is a number and secondNum is not overloaded
+        else if (!isNaN(input) && secondNum.length < 9) {
+            secondNum += input;
+            displayValue = secondNum;
+            updateDisplay();
+        }
+        // If input is an operator
+        else if (operators.includes(input)) {
+            let result = operate().toString();
+            clearData()
+            firstNum = result;
+            if (input !== "=") {
+                operator = input;
+                currStep = "operator";
+            }
+            displayValue = firstNum;
+            updateDisplay();
+        }
+    }
+    printData();
+}
 
+const operate = () => {
+    const num1 = parseFloat(firstNum);
+    const num2 = parseFloat(secondNum);
+    if (operator === "+") {
+        return num1 + num2;
     }
-    if (currStep === "secondNum") {
-        
+    else if (operator === "-") {
+        return num1 - num2;
     }
-    if (!isNaN(input)) {
-        return parseFloat(input);
+    else if (operator === "×") {
+        return num1 * num2;
     }
+    else if (operator === "÷") {
+        return num1 / num2;
+    }
+    else {
+        alert("ERROR: operate() called on invalid operator")
+    }
+}
 
+const updateDisplay = () => {
+    displayScreen.innerText = displayValue;
+}
+
+const clearData = () => {
+    displayValue = "";
+    firstNum = "";
+    secondNum = "";
+    operator = "";
+    currStep = "firstNum";
+}
+
+// For debugging
+const printData = () => {
+    console.log("NUM1:", firstNum, "NUM2:", secondNum, "OP:", 
+    operator, "CURRSTEP:", currStep, "DISPLAYVAL:", displayValue)
 }
 
 main()
